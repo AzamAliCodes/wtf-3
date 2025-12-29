f0ad6820f9a215403c96871a0711aaefaa8e19d4
AzamAliCodes <social.azamali@gmail.com>
2025-12-30 02:01:08 +0530

    Remove Netflix loader

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,14 +1,4 @@
-import NetflixLoader from "@/components/NetflixLoader";
-
 export default function Loading() {
-  // This version of the loader manages its own lifecycle, but
-  // we still use loading.tsx as the entry point for Next.js
-  // The loader will call onComplete after its animation, but since
-  // we aren't passing that prop here, it will just play and then
-  // the page will appear when ready.
-  return (
-    <div className="h-screen w-screen">
-      <NetflixLoader />
-    </div>
-  );
+  // You can add any UI inside Loading, including a Skeleton.
+  return <div className="h-screen w-screen"></div>;
 }
